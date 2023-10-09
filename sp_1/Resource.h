dfc34b6364937a302ac735fe2636440f4da069d7
Denis <bilalzasimauskaite@gmail.com>
2023-10-09 22:59:12 +0300

    Final version

--- sp_1/Resource.h
+++ sp_1/Resource.h
@@ -1,30 +1,36 @@
 ﻿//{{NO_DEPENDENCIES}}
 // Включаемый файл, созданный в Microsoft Visual C++.
 // Используется sp_1.rc
-
-#define IDS_APP_TITLE			103
-
-#define IDR_MAINFRAME			128
-#define IDD_SP1_DIALOG	102
-#define IDD_ABOUTBOX			103
-#define IDM_ABOUT				104
-#define IDM_EXIT				105
-#define IDI_SP1			107
-#define IDI_SMALL				108
-#define IDC_SP1			109
-#define IDC_MYICON				2
-#ifndef IDC_STATIC
-#define IDC_STATIC				-1
-#endif
-// Следующие стандартные значения для новых объектов
 //
+#define IDC_MYICON                      2
+#define IDD_SP1_DIALOG                  102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_SP1                         107
+#define IDI_SMALL                       108
+#define IDC_SP1                         109
+#define IDR_MAINFRAME                   128
+#define IDB_BITMAP1                     132
+#define IDB_BITMAP2                     133
+#define IDB_BITMAP3                     134
+#define IDB_BITMAP4                     135
+#define IDB_BITMAP5                     136
+#define IDB_BITMAP6                     137
+#define IDB_BITMAP7                     138
+#define IDB_BITMAP8                     139
+#define IDB_BITMAP9                     140
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NO_MFC					130
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_COMMAND_VALUE		32771
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		110
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        141
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
--- sp_1/sp_1.cpp
+++ sp_1/sp_1.cpp
@@ -4,12 +4,124 @@
 #include "framework.h"
 #include "sp_1.h"
 
+// library for ALPHABLEND
+#pragma comment(lib, "Msimg32.lib")
+
+
 #define MAX_LOADSTRING 100
+
+#define IDT_TIMER_UP  1
+#define IDT_TIMER_DOWN 2
+#define IDT_TIMER_RIGHT 3
+#define IDT_TIMER_LEFT 4
+
+
+
+
 
 // Глобальные переменные:
 HINSTANCE hInst;                                // текущий экземпляр
 WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
 WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+// my Global Variables
+HBITMAP hBmp;
+
+class Sprite {
+
+public:
+    int x = 200;
+    int y = 100;
+    int width = 100;
+    int height = 137;
+    CONST int SPEED = 20;
+
+    BOOL inBounce = FALSE;
+    CONST int BOUNCE_SPEED = 20;
+    CONST int BOUNCE_LENGTH = 80;
+
+
+    BOOL isBmp = FALSE;
+
+    void moveUp(LONG topBound, HWND hwnd) {
+        if (y - SPEED <= topBound)
+        {
+            y = 0;
+            InvalidateRect(hwnd, NULL, TRUE);
+
+
+            SetTimer(hwnd,             // handle to main window 
+                IDT_TIMER_UP,            // timer identifier 
+                40,                 // 10-second interval 01.sec 
+                (TIMERPROC)NULL);     // no timer callback
+            inBounce = TRUE;
+
+        }
+        else
+        {
+            y -= SPEED;
+        }
+    }
+
+    void moveDown(LONG downBound, HWND hwnd) {
+
+        if (y + height + SPEED >= downBound)
+        {
+            y = downBound - height;
+            InvalidateRect(hwnd, NULL, TRUE);
+
+            SetTimer(hwnd, IDT_TIMER_DOWN, 40, (TIMERPROC)NULL);
+            inBounce = TRUE;
+
+        }
+        else
+        {
+            y += SPEED;
+        }
+
+    }
+
+    void moveRight(LONG rightBound, HWND hwnd) {
+        if (x + width + SPEED >= rightBound)
+        {
+            x = rightBound - width;
+            InvalidateRect(hwnd, NULL, TRUE);
+
+            SetTimer(hwnd, IDT_TIMER_RIGHT, 40, (TIMERPROC)NULL);   
+            inBounce = TRUE;
+
+        }
+        else
+        {
+            x += SPEED;
+        }
+    }
+
+    void moveLeft(LONG leftBound, HWND hwnd) {
+        if (x - SPEED <= leftBound)
+        {
+            x = 0;
+            InvalidateRect(hwnd, NULL, TRUE);
+
+
+            SetTimer(hwnd, IDT_TIMER_LEFT, 40, (TIMERPROC)NULL);
+            inBounce = TRUE;
+
+        }
+        else
+        {
+            x -= SPEED;
+        }
+    }
+
+};
+
+Sprite sprite;
+
+
+
+
+
+
 
 // Отправить объявления функций, включенных в этот модуль кода:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -75,7 +187,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.hInstance      = hInstance;
     wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SP1));
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.hbrBackground  = CreateSolidBrush(RGB(80, 110, 78));
     wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_SP1);
     wcex.lpszClassName  = szWindowClass;
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
@@ -111,6 +223,12 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    return TRUE;
 }
 
+
+
+
+
+
+
 //
 //  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
 //
@@ -121,6 +239,9 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //  WM_DESTROY  - отправить сообщение о выходе и вернуться
 //
 //
+
+
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
@@ -142,12 +263,157 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             }
         }
         break;
+
+    case WM_CREATE:
+        {
+            // Load bitmap to the global variable only once 
+            hBmp = (HBITMAP)LoadImage(NULL, L"../sprite_witch.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+            if (hBmp == NULL)
+            {
+                // Show message if bitmap wasn`t found
+                ::MessageBox(NULL, __T("Load image failed"), __T("Error"), MB_OK);
+            }
+            else
+            {
+                // Remember that bitmap wasn`t found so then draw rectangle instead
+                sprite.isBmp = true;
+            }
+
+            sprite.x = 200;
+            sprite.y = 200;
+
+        }
+        break;
+    case WM_GETMINMAXINFO:
+        {
+            MINMAXINFO* pMinMax = (MINMAXINFO*)lParam;
+
+            pMinMax->ptMinTrackSize.x = 1200;
+            pMinMax->ptMinTrackSize.y = 837;
+        }
+        break;
+    case WM_TIMER:
+        {
+
+            RECT rcClientRect;
+            GetClientRect(hWnd, &rcClientRect);
+
+            switch (wParam)
+            {
+
+                case IDT_TIMER_UP:
+                    if (sprite.y == rcClientRect.top + sprite.BOUNCE_LENGTH)
+                    {
+                        KillTimer(hWnd, IDT_TIMER_UP);
+                        sprite.inBounce = FALSE;
+                    }
+                    else {
+                        sprite.y += sprite.BOUNCE_SPEED;
+                        InvalidateRect(hWnd, NULL, TRUE);
+                    }
+                    return 0;
+
+                case IDT_TIMER_DOWN:
+                    if (sprite.y == rcClientRect.bottom - sprite.height - sprite.BOUNCE_LENGTH)
+                    {
+                        KillTimer(hWnd, IDT_TIMER_DOWN);
+                        sprite.inBounce = FALSE;
+                    }
+                    else
+                    {
+                        sprite.y -= sprite.BOUNCE_SPEED;
+                        InvalidateRect(hWnd, NULL, TRUE);
+                    }
+                    return 0;
+
+                case IDT_TIMER_RIGHT:
+                    if (sprite.x == rcClientRect.right - sprite.width - sprite.BOUNCE_LENGTH)
+                    {
+                        KillTimer(hWnd, IDT_TIMER_RIGHT);
+                        sprite.inBounce = FALSE;
+                    }
+                    else
+                    {
+                        sprite.x -= sprite.BOUNCE_SPEED;
+                        InvalidateRect(hWnd, NULL, TRUE);
+                    }
+                    return 0;
+
+                case IDT_TIMER_LEFT:
+                    if (sprite.x == rcClientRect.left + sprite.BOUNCE_LENGTH)
+                    {
+                        KillTimer(hWnd, IDT_TIMER_LEFT);
+                        sprite.inBounce = FALSE;
+                    }
+                    else
+                    {
+                        sprite.x += sprite.BOUNCE_SPEED;
+                        InvalidateRect(hWnd, NULL, TRUE);
+                    }
+                    return 0;
+            }
+        }
+        break;
+    case WM_MOUSEWHEEL:
+        {
+
+       
+        if (sprite.inBounce == false)
+        {
+
+            int wheelDelta = GET_WHEEL_DELTA_WPARAM(wParam);
+
+            RECT rcClientRect;
+            GetClientRect(hWnd, &rcClientRect);
+
+            if (LOWORD(wParam) == MK_SHIFT)
+            {
+
+                if (wheelDelta > 0)
+                {
+                    sprite.moveLeft(rcClientRect.left, hWnd);
+                }
+                else
+                {
+                    sprite.moveRight(rcClientRect.right, hWnd);
+                }
+            }
+            else
+            {
+
+
+                if (wheelDelta > 0)
+                {
+                    sprite.moveUp(rcClientRect.top, hWnd);
+                }
+                else
+                {
+                    sprite.moveDown(rcClientRect.bottom, hWnd);
+                }
+            }
+
+            InvalidateRect(hWnd, NULL, TRUE);
+        }
+
+        }
+        break;
     case WM_PAINT:
         {
+        
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
             // TODO: Добавьте сюда любой код прорисовки, использующий HDC...
+
+            HDC bmpDC = CreateCompatibleDC(hdc);
+            SelectObject(bmpDC, hBmp);
+            BLENDFUNCTION blendf = { AC_SRC_OVER, 0, 255, AC_SRC_ALPHA };  
+            AlphaBlend(hdc, sprite.x, sprite.y, sprite.width, sprite.height, bmpDC, 0, 0, 100, 137, blendf);
+            DeleteDC(bmpDC);
+            //DeleteObject(hBmp);
+
             EndPaint(hWnd, &ps);
+
+           
         }
         break;
     case WM_DESTROY:
